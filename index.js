45520f37d82c2cd4b171a6955299c990cf173308
yeasin550 <yeasin51570@gmail.com>
2023-05-03 10:42:04 +0600

    install cors and send data client site

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express();
 const port = 5000;
 
-// app.use(cors())
+app.use(cors())
 
 const recipe = require('./recipe.json')
 app.get('/', (req, res) => {
